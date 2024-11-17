ac69d563f0ea33741150cc2eeee78ef52fc9abb4
hieubk <hieundpython@gmail.com>
2024-11-17 19:04:40 +0700

    add evaluator

--- object/enviroment.go
+++ object/enviroment.go
@@ -2,16 +2,28 @@ package object
 
 type Enviroment struct {
 	store map[string]Object
+	outer *Enviroment
 }
 
 func NewEnviroment() *Enviroment {
 	s := make(map[string]Object)
 
-	return &Enviroment{store: s}
+	return &Enviroment{store: s, outer: nil}
+}
+
+func NewEnclosedEnviroment(outer *Enviroment) *Enviroment {
+	env := NewEnviroment()
+	env.outer = outer
+
+	return env
 }
 
 func (e *Enviroment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.store[name]
+	}
 
 	return obj, ok
 }
